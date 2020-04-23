df39193232ba857294fe1b6299ecaee819344688
André Aubin <andre@andral.xyz>
2020-04-23 19:46:10 +0200

    Adding a simpler, custom locale system, rather than next-i18-next

---
+++ frontend/i18n/i18n.ts
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+import Router from 'next/router'
+
+
+// const withTranslation = (args: any) => {
+//   return (Wrapper: any) => Wrapper
+// }
+
+// const i18n = {
+//   language: 'trololo'
+// }
+
+// export {
+//   Link,
+//   Router,
+//   withTranslation,
+//   i18n
+// }
+
+// export default {
+//   Link,
+//   Router,
+//   withTranslation,
+//   i18n
+// }
