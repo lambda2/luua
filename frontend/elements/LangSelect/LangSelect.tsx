6df2f59d3d5c9ed9d96b30eb3cc38dd767d36953
André Aubin <andre@andral.xyz>
2020-04-25 14:09:22 +0200

    Remove useless components

--- frontend/elements/LangSelect/LangSelect.tsx
+++ frontend/elements/LangSelect/LangSelect.tsx
@@ -1,24 +1,24 @@
 import React from 'react'
-import { i18n } from '../../i18n/i18n'
+// import { i18n } from '../../i18n/i18n'
 import { Button } from 'antd'
 
 const LangSelect = ({ currentLanguage }: { currentLanguage: string }) => {
-  const lang = currentLanguage || i18n.language
+  // const lang = currentLanguage || i18n.language
   return (
     <div className="language-select">
-      <Button
+      {/* <Button
         type='link'
         onClick={() => i18n.changeLanguage(lang === 'en' ? 'fr' : 'en')}
       >
         {lang === 'en' ? '🇫🇷 Francais' : '🇬🇧 English'}
-      </Button>
+      </Button> */}
     </div>
   )
 }
 
-LangSelect.getInitialProps = async ({ req }: any) => {
-  const currentLanguage = req ? req.language : i18n.language
-  return { currentLanguage }
-}
+// LangSelect.getInitialProps = async ({ req }: any) => {
+//   const currentLanguage = req ? req.language : i18n.language
+//   return { currentLanguage }
+// }
 
 export default LangSelect
