9d9aad7474ae1664c1d998e0c6b707935db534db
André Aubin <andre@andral.xyz>
2020-04-29 00:36:44 +0200

    Fix: Workspace header for mission form

--- frontend/pages/manage/[workspace_id]/missions/new.tsx
+++ frontend/pages/manage/[workspace_id]/missions/new.tsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { withAuthSync } from '../../../../utils/auth'
 import MissionSetup from '../../../../components/MissionSetup/MissionSetup'
 import ContentLayout from '../../../../layouts/ContentLayout/ContentLayout'
 import ManageLeftMenu from '../../../../layouts/ManageLeftMenu/ManageLeftMenu'
+import WorkspaceContext from '../../../../contexts/WorkspaceContext'
+import WorkspaceHeader from '../../../../components/WorkspaceHeader/WorkspaceHeader'
+import Link from 'next/link'
 
 const NewMission = (props: any) => {
 
-  return (
+  const { currentWorkspace } = useContext(WorkspaceContext)
+
+  return (<>
+    {currentWorkspace && <WorkspaceHeader
+      workspace={currentWorkspace}
+      active='missions'
+    />}
     <ContentLayout>
       <MissionSetup />
     </ContentLayout>
-  )
+  </>)
 }
 
 export default withAuthSync(NewMission)
