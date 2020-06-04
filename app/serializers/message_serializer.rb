5fb7b50e16866f0c9698281cc9915e004967e53f
André Aubin <andre@andral.xyz>
2020-06-04 12:28:45 +0200

    Fix: tests

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -29,7 +29,7 @@ class MessageSerializer < Panko::Serializer
   attributes :id, :content, :parent_id, :user_id, :discussion_id,
              :created_at, :updated_at, :workspace_id,
              :positive_vote_count, :negative_vote_count,
-             :serialized_content
+             :serialized_content, :message_type
 
   has_one :user, serializer: UserLightSerializer
   has_one :parent, serializer: MessageSerializer
